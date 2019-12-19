a0015e422038d62659dc0d5af4a949c8f49659ed
Kyrian <kyrian.castel@protonmail.com>
2019-12-19 16:52:38 +0100

    Updates

---
+++ precache-manifest.d387c120e72120d47bde0e8c87c8de1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4774bb8c6885fc6baf687d33f2b91085",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "f23d9858079980e45399",
+    "url": "/recipe/static/css/main.eaffaac6.chunk.css"
+  },
+  {
+    "revision": "4264144ee313864d7a56",
+    "url": "/recipe/static/js/2.58e90072.chunk.js"
+  },
+  {
+    "revision": "f23d9858079980e45399",
+    "url": "/recipe/static/js/main.3f001b6b.chunk.js"
+  },
+  {
+    "revision": "6ca6840731ee65279f96",
+    "url": "/recipe/static/js/runtime-main.41e0d74a.js"
+  },
+  {
+    "revision": "bfdbacc8ea97f71b05dcfea811fc4c97",
+    "url": "/recipe/static/media/backcground.bfdbacc8.jpg"
+  }
+]);
